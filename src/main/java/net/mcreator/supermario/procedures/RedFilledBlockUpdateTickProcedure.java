246f4453d72d4f5b7e35e7a5c64299f790ba062f
pploh <Naten@DESKTOP-M778PA5.dean.edu>
2023-09-19 01:39:18 -0400

    On and Off Block Fix

--- src/main/java/net/mcreator/supermario/procedures/RedFilledBlockUpdateTickProcedure.java
+++ src/main/java/net/mcreator/supermario/procedures/RedFilledBlockUpdateTickProcedure.java
@@ -1,29 +1,32 @@
 package net.mcreator.supermario.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.core.BlockPos;
 
-import javax.annotation.Nullable;
+import net.mcreator.supermario.network.SuperMarioModVariables;
+import net.mcreator.supermario.init.SuperMarioModBlocks;
+
+import java.util.Map;
 
 public class RedFilledBlockUpdateTickProcedure {
-public static void execute(
-LevelAccessor world,
-double x,
-double y,
-double z
-) {
-if (==false&&==true) {{
-BlockPos _bp = new BlockPos(x,y,z);
-BlockState _bs = SuperMarioModBlocks.RED_OUTLINE_BLOCK.get().defaultBlockState();
-BlockState _bso = world.getBlockState(_bp);
-for(Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-if (_property != null && _bs.getValue(_property) != null)
-try {
-_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-} catch (Exception e) {}
-}
-world.setBlock(_bp, _bs, 3);
-}
-}
-}
+	public static void execute(LevelAccessor world, double x, double y, double z) {
+		if (SuperMarioModVariables.MapVariables.get(world).OFF_Block_Active == true) {
+			{
+				BlockPos _bp = new BlockPos(x, y, z);
+				BlockState _bs = SuperMarioModBlocks.RED_OUTLINE_BLOCK.get().defaultBlockState();
+				BlockState _bso = world.getBlockState(_bp);
+				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
+					if (_property != null && _bs.getValue(_property) != null)
+						try {
+							_bs = _bs.setValue(_property, (Comparable) entry.getValue());
+						} catch (Exception e) {
+						}
+				}
+				world.setBlock(_bp, _bs, 3);
+			}
+		}
+	}
 }
